c7a502b7cc00a10d84b1e19ed986c470e7cd674b
Marcy Silverman <marcysilve225@gmail.com>
2019-03-21 11:09:43 -0700

    first commit

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase-firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyB3n7Y9uk50YTtfPaJmryRWwKFgUMCWHf8",
+    authDomain: "fir-project-5d9f4.firebaseapp.com",
+    databaseURL: "https://fir-project-5d9f4.firebaseio.com",
+    projectId: "fir-project-5d9f4",
+    storageBucket: "fir-project-5d9f4.appspot.com",
+    messagingSenderId: "704367083713"
+});
+
+export const app = firebase;
+
+export const firestore = app.firestore();
+
+export const notesCollection = firestore.collection('notes');
